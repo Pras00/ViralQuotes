7307c020a86d1509d9c3cd9406c221d350cc5d73
Refin Dhani <93148655+RefinDhani27@users.noreply.github.com>
2024-02-26 08:10:35 +0700

    Add files via upload

---
+++ Sales Page/array_image.php
@@ -0,0 +1,7 @@
+<?php
+
+    $image = [
+        "judul_modul" => ["Meeting Invitation"],
+        "gambar" => ["meeting1", "meeting2", "meeting3", "meeting4", "meeting5", "meeting6", "meeting7", "meeting8"],
+    ];
+?>
